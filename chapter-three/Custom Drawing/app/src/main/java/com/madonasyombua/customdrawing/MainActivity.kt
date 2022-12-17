c8c9ac982003f1115109bd403f920f4ad26e12c2
madonahs <syombuamadona@gmail.com>
2022-12-17 07:40:52 +0300

    chapter 3

---
+++ chapter-three/Custom Drawing/app/src/main/java/com/madonasyombua/customdrawing/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.madonasyombua.customdrawing
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.madonasyombua.customdrawing.ui.theme.CustomDrawingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomDrawingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CreateCircleShape()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateCircleShape() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val setCanvasWidth = size.width
+        val setCanvasHeight = size.height
+        drawCircle(
+            color = Color.Red,
+            center = Offset(x = setCanvasWidth / 2, y = setCanvasHeight / 2),
+            radius = size.minDimension / 4
+        )
+    }
+}
