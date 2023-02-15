16d447f071448121e003e6e06f5778d95bbdde47
madonahs <syombuamadona@gmail.com>
2023-02-15 10:18:23 -0500

    move sample login to chapter 3

--- chapter-two/Sample Login/app/src/main/java/com/madonasyombua/samplelogin/LoginApp.kt
+++
@@ -1,7 +0,0 @@
-package com.madonasyombua.samplelogin
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class LoginApp : Application()
--- chapter-two/Sample Login/app/src/main/java/com/madonasyombua/samplelogin/util/Destination.kt
+++
@@ -1,6 +0,0 @@
-package com.madonasyombua.samplelogin.util
-
-sealed class Destination (val route: String){
-    object LoginScreen: Destination("login_screen")
-    object RegisterScreen: Destination("register_screen")
-}
