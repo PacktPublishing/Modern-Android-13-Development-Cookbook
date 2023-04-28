2c094e2d8e52b90b671c64a18e7aceaf1c1e175c
madonahs <syombuamadona@gmail.com>
2023-04-28 12:11:25 -0400

    upgrade versions

--- chapter-two/ComposeBasics/app/src/main/java/com/madonasyombua/composebasics/MainActivity.kt
+++ chapter-two/ComposeBasics/app/src/main/java/com/madonasyombua/composebasics/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.madonasyombua.composebasics
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.madonasyombua.composebasics.components.ButtonWithIcon
+import com.madonasyombua.composebasics.components.CornerCutShapeButton
+import com.madonasyombua.composebasics.components.EditTextExample
+import com.madonasyombua.composebasics.components.ElevatedButtonExample
+import com.madonasyombua.composebasics.components.ImageViewExample
+import com.madonasyombua.composebasics.components.NotOutlinedEditTextExample
+import com.madonasyombua.composebasics.components.RoundCornerShapeButton
+import com.madonasyombua.composebasics.pagerexample.CityTabCarousel
 import com.madonasyombua.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +34,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxSize()
+                            .padding(start = 12.dp, end = 12.dp)
+                    ) {
+                        EditTextExample()
+                        NotOutlinedEditTextExample()
+                        ButtonWithIcon()
+                        CornerCutShapeButton()
+                        RoundCornerShapeButton()
+                        ElevatedButtonExample()
+                        ImageViewExample()
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeBasicsTheme {
-        Greeting("Android")
     }
 }
